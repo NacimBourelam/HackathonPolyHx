3c9e540bd5f9c4c53257325aed3ddc41d68fd0b8
NacimBourelam <112643886+NacimBourelam@users.noreply.github.com>
2024-02-04 10:05:03 -0500

    Refactor marker creation and routing logic

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-let markers = [];
-let routingControls = [];
+var markers = [];
 var selectedMarkerIcon = L.icon({
     iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
     iconSize: [25, 41],
@@ -22,117 +21,82 @@ var defaultMarkerIcon = L.icon({
     shadowSize: [41, 41]
 });
 
-// var points = [
-//     { name: "Point A", coordinates: [45.508, -73.567] },
-//     { name: "Point B", coordinates: [45.52, -73.55] },
-//     { name: "Point C", coordinates: [45.495, -73.58] },
-//     { name: "Point D", coordinates: [45.51, -73.55] },
-//     { name: "Point E", coordinates: [45.515, -73.565] },
-//     { name: "Point XD", coordinates: [45.615, -73.565] }
-// ];
+var points = [
+    { name: "Point A", coordinates: [45.508, -73.567] },
+    { name: "Point B", coordinates: [45.52, -73.55] },
+    { name: "Point C", coordinates: [45.495, -73.58] },
+    { name: "Point D", coordinates: [45.51, -73.55] },
+    { name: "Point E", coordinates: [45.515, -73.565] }
+];
 
 var distances = [];
 
-// points.forEach(function(point) {
-//     // var marker = L.marker(point.coordinates, { icon: defaultMarkerIcon }).addTo(map);
-//     // marker.bindPopup(point.name);
+for (var i = 0; i < points.length; i++) {
+    for (var j = i + 1; j < points.length; j++) {
+        getDistanceBetweenTwoPoints(points[i].coordinates, points[j].coordinates, i, j);
+    }
+}
 
+points.forEach(function(point) {
+    var marker = L.marker(point.coordinates, { icon: defaultMarkerIcon }).addTo(map);
+    marker.bindPopup(point.name);
+
+    marker.on('click', function(event) {
+        if (markers.length < 2) {
+            markers.push(marker);
+            marker.setIcon(selectedMarkerIcon);
+            if (markers.length === 2) {
+                routingControl = L.Routing.control({
+                    waypoints: [
+                        markers[0].getLatLng(),
+                        markers[1].getLatLng()
+                    ],
+                    routeWhileDragging: true
+                }).addTo(map);
+
+                routingControl.on('routesfound', function(e) {
+                    var routes = e.routes;
+                    var distance = routes[0].summary.totalDistance / 1000; // Distance en Km
+                    console.log("Distance entre les marqueurs:", markers[0]._popup._content, markers[1]._popup._content, distance.toFixed(2), "Km");
+                });
+            }
+        } else {
+            markers[0].setIcon(defaultMarkerIcon);
+            markers.shift();
+            markers.push(marker);
+            marker.setIcon(selectedMarkerIcon);
+            routingControl.setWaypoints([
+                markers[0].getLatLng(),
+                markers[1].getLatLng()
+            ]);
+        }
+    });
     
-// });
-
-let counter = 0;
-// let routingControl = L.Routing.control({})
-map.on('click', (e)=>{
-    console.log("test")
-    let marker = L.marker(e.latlng  , { icon: defaultMarkerIcon }).addTo(map);
-    marker.bindPopup(counter++);
-    markers.push(marker)
-
-    let routingControl = L.Routing.control({
-        waypoints : markers.map((v)=>v.getLatLng()),
-        routeWhileDragging: true,
-    }).addTo(map)
-    routingControls.push(routingControl)
-
-    // marker.on('click', (event) => {
-        // if (markers.length < 2) {
-            // markers.push(marker);
-            // marker.setIcon(selectedMarkerIcon);
-            // if (markers.length === 2) {
-            //     routingControl = L.Routing.control({
-            //         waypoints: [
-            //             markers[0].getLatLng(),
-            //             markers[1].getLatLng()
-            //         ],
-            //         routeWhileDragging: true
-            //     }).addTo(map);
-
-            //     routingControl.on('routesfound', function(e) {
-            //         var routes = e.routes;
-            //         var distance = routes[0].summary.totalDistance / 1000; // Distance en Km
-            //         console.log("Distance entre les marqueurs:", markers[0]._popup._content, markers[1]._popup._content, distance.toFixed(2), "Km");
-            //         distances.push({
-            //             from: markers[0]._popup._content,
-            //             to: markers[1]._popup._content,
-            //             distance: distance
-            //         });
-            //     });
-            // }
-        // } else {
-        //     markers[0].setIcon(defaultMarkerIcon);
-        //     markers.shift();
-        //     markers.push(marker);
-        //     marker.setIcon(selectedMarkerIcon);
-        //     routingControl.setWaypoints([
-        //         markers[0].getLatLng(),
-        //         markers[1].getLatLng()
-        //     ]);
-        // }
-    // });
-})
-
-
-document.getElementById('reset-button').addEventListener('click', (e)=>{
-    markers.forEach((v)=>{
-        v.removeFrom(map)
-    })
-    routingControls.forEach((v)=>{
-        v.remove()
-    })
-    markers = []
-    routingControls = []
 });
 
-document.getElementById('optimize-button').addEventListener('click', (e)=>{
-    let pointsToSend = markers.map((v)=>{
-        let coords = v.getLatLng()
-        return {x : coords.lat, y : coords.lng}
-    })
-    fetch("http://localhost:3000/map", {
-        method: 'POST',
-        body: JSON.stringify(pointsToSend)
-    }).then((response)=>response.json()).then(
-        (points) => {
-            console.log(points)
-            markers.forEach((v)=>{
-                v.removeFrom(map)
-            });
-            routingControls.forEach((v)=>{
-                v.remove()
-            })
-            markers = []
-            routingControls = []
-            points.Points.forEach((point, index)=>{
-                let marker = L.marker({lat:point.x, lng:point.y}  , { icon: defaultMarkerIcon }).addTo(map);
-                marker.bindPopup(counter++);
-                markers.push(marker)
 
-            })    
-            let routingControl = L.Routing.control({
-                waypoints : markers.map((v)=>v.getLatLng()),
-                routeWhileDragging: true,
-            }).addTo(map)
-            routingControls.push(routingControl);
+async function getDistanceBetweenTwoPoints(pointA, pointB, indexA, indexB) {
+    var response = await fetch(`http://router.project-osrm.org/route/v1/driving/${pointA[1]},${pointA[0]};${pointB[1]},${pointB[0]}?overview=false`);
+    var data = await response.json();
+    var distance = data.routes[0].distance;
+    distances.push({ indices: [indexA, indexB], distance: distance });
+    console.log("Distance entre", points[indexA].name, "et", points[indexB].name, ":", distance.toFixed(2), "m");
+// save to local storage the distances between the points and the names of the points
+    localStorage.setItem('distances', JSON.stringify(distances));
+    localStorage.setItem('points', JSON.stringify(points));
+
+    //write une matrice de distance avec le nom des points en ligne et en colonne
+    var distanceMatrix = [];
+    for (var i = 0; i < points.length; i++) {
+        distanceMatrix[i] = [];
+        for (var j = 0; j < points.length; j++) {
+            distanceMatrix[i][j] = 0;
         }
-    )
-})
+    }
+    distances.forEach(function(distance) {
+        distanceMatrix[distance.indices[0]][distance.indices[1]] = distance.distance;
+        distanceMatrix[distance.indices[1]][distance.indices[0]] = distance.distance;
+    });
+
+    console.log("Matrice de distance:", distanceMatrix);
+}
